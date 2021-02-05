06a79b5b93f1653b75369af90ee18b993520cd49
nina.oh <nina.oh@kakaocorp.com>
2021-02-05 13:45:37 +0900

    Format files

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -2,19 +2,18 @@ import { Heading } from 'rebass'
 import { Hero } from 'react-landing-page'
 
 function Intro() {
-    return (
-        <Hero
-        color="black"
-        bg="white"
-        backgroundImage="https://source.unsplash.com/jxaj-UrzQbc/1600x900"
-      >
-          <Heading>Name of your app</Heading>
-          {/* <Subhead>a couple more words</Subhead> */}
-          {/* <CallToAction href="/getting-started" mt={3}>Get Started</CallToAction> */}
-          {/* <ScrollDownIndicator/> */}
-      </Hero>
-    )
-
+  return (
+    <Hero
+      color="black"
+      bg="white"
+      backgroundImage="https://source.unsplash.com/jxaj-UrzQbc/1600x900"
+    >
+      <Heading>Name of your app</Heading>
+      {/* <Subhead>a couple more words</Subhead> */}
+      {/* <CallToAction href="/getting-started" mt={3}>Get Started</CallToAction> */}
+      {/* <ScrollDownIndicator/> */}
+    </Hero>
+  );
 }
 
 export default Intro;
