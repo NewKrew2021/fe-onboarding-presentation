f1d8874feb6c922cf018bd1131ee02169ec92f2c
nina.oh <nina.oh@kakaocorp.com>
2021-02-05 13:27:59 +0900

    Add component directory

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import { Heading, Subhead } from 'rebass'
-import {
-  Hero, CallToAction, ScrollDownIndicator
-} from 'react-landing-page'
+import Intro from './components/Intro';
+
 
 function App() {
   return (
     <div className="App">
-      <Hero
-        color="black"
-        bg="white"
-        backgroundImage="https://source.unsplash.com/jxaj-UrzQbc/1600x900"
-      >
-          <Heading>Name of your app</Heading>
-          {/* <Subhead>a couple more words</Subhead> */}
-          {/* <CallToAction href="/getting-started" mt={3}>Get Started</CallToAction> */}
-          {/* <ScrollDownIndicator/> */}
-      </Hero>
+      <Intro/>
     </div>
   );
 }
---
+++ src/components/Intro.js
@@ -0,0 +1,20 @@
+import { Heading } from 'rebass'
+import { Hero } from 'react-landing-page'
+
+function Intro() {
+    return (
+        <Hero
+        color="black"
+        bg="white"
+        backgroundImage="https://source.unsplash.com/jxaj-UrzQbc/1600x900"
+      >
+          <Heading>Name of your app</Heading>
+          {/* <Subhead>a couple more words</Subhead> */}
+          {/* <CallToAction href="/getting-started" mt={3}>Get Started</CallToAction> */}
+          {/* <ScrollDownIndicator/> */}
+      </Hero>
+    )
+
+}
+
+export default Intro;
